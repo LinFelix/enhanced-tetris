fb65938452a1e507b338bcb48752f83666a5a5e2
LinFelix <felix-voelker@gmx.net>
2017-03-17 17:23:45 +0100

    Ananass, started enhandced_tetris.el

---
+++ ehanced_tetris.el
@@ -0,0 +1,26 @@
+;;; enhanced_tetris.el --- enhandce Tetris for Emacs
+
+;; Author: Felix Völker(LinFelix)
+;; Version: 0.0
+;; Created: 2017-03-17
+;; Keywords: games
+
+;; This file is puplished under the GPL v3
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'tetris)
+
+;; customization variables ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defgroup enhanced-tetris nil
+  "normal Tetris is too normal now, Ananass"
+  :prefix "ananass-tetris-"
+  :group 'games)
+
+(provide 'enhanced_tetris)
+
+;;; enhanced_tetris.el ends here
+
